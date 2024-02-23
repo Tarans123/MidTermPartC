fe944a5fa4abf4d8ad788c98f1dd8a8b4d750955
Taranpreet Singh <taran>
2024-02-23 16:22:57 -0500

    subjects favourite

---
+++ src/midtermexam_versiona_extensioncode/Taranpreet_Design.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midtermexam_versiona_extensioncode;
+
+/**
+ *
+ * @author taranpreetsingh
+ */
+public class Taranpreet_Design {
+    
+    //favourite subject is fundamentals of software design and implementation
+    
+    //Database management,enterprise java
+    
+    
+    
+}
